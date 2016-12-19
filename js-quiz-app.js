f18955d2dd707e27f100121fc2facffe07f486ad
maria-S-456 <mars381pika@gmail.com>
2016-12-19 08:53:25 -0500

    second commit

--- js-quiz-app.js
+++ js-quiz-app.js
@@ -12,16 +12,23 @@ var questions = {
 	[
 		"What makes Pluto's orbit different from the rest of the planets?", "It wobbles.", "It crosses Neptune's orbit", "It crosses Uranus's orbit", "It is perfectly circular."]
 }; //JSON object
+
+var testQuestions = {["which dwarf planet has the furthest orbit?"], ["Why does Haumea have a football-like appearance?"],["What is NOT a criteria to be called a planet?"],["What is the closest dwarf planet?"],["What makes Pluto's orbit different from the rest of the planets?"]};
+var firstOption = {["Pluto"],["It is heavily bombarded by asteroids, which have altered it's appearance."],["It must have an atmosphere."],["Pluto"],["It wobbles."]};
+var secondOption = {["Makemake"],["It's moons' strong gravity has caused it to stretch."],["It orbits the sun."],["Eris"],["It crosses Neptune's orbit."]};
+var thirdOption = {["Sedna"],["It's gravity is not strong enough to keep it in a spherical shape."],["It is round in shape."],["Ceres"],["It crosses Uranus's orbit"]};
+var fourthOption = {["Haumea"],["It's rotation is extremely fast, giving it an elongated shape."],["There are no foreign objects in it's orbit."],["Makemake"],["It is perfectly circular."]};
 $(document).ready(function() {
-	alert('Form loaded');
-
-
 	$('#submitBtn').click(function()
 	{
 		$("p").removeClass("center");
 		$("ul").removeClass("center");
-		$("button").addClass("center");
-		$("#first").text("blue");
+		$("ul").removeClass("hidden");
+		$("p").text(testQuestions[x]);
+		$("#first").text(firstOption[x]);
+		$("#second").text(secondOption[x]);
+		$("#third").text(thirdOption[x]);
+		$("#fourth").text(fourthOption[x]);
 	});
 
 });
